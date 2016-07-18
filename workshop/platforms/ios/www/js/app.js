f1544522ce9dbfbe20860640523238d4bef7bfbe
Smart Guy <andrei.innovative82@yandex.com>
2016-07-18 03:17:01 +0200

    working with handlebars, applied tpl for index and search

---
+++ workshop/platforms/ios/www/js/app.js
@@ -0,0 +1,40 @@
+// We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
+(function () {
+
+    var homeTpl = Handlebars.compile($("#home-tpl").html());
+    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+
+    /* ---------------------------------- Local Variables ---------------------------------- */
+    var service = new EmployeeService();
+    service.initialize().done(function () {
+        renderHomeView();
+    });
+
+    /* ---------------------------------- Local Functions ---------------------------------- */
+    function findByName() {
+        service.findByName($('.search-key').val()).done(function (employees) {
+            $('.content').html(employeeListTpl(employees));
+        });
+    }
+
+    document.addEventListener('deviceready', function() {
+        if (navigator.notification) {
+            window.alert = function ( message ) {
+                navigator.notification.alert(
+                    message, 
+                    null,
+                    "Workshop", 
+                    "Ok"
+                );
+            };
+        }
+        FastClick.attach(document.body);
+    }, false);
+
+    function renderHomeView() {
+
+        $('body').html(homeTpl());
+        $('.search-key').on('keyup', findByName);
+    }
+
+}());
